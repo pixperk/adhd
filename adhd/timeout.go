e5f86c3502a9b8960aa22a804254425817cbdb79
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-09-20 13:04:09 +0530

    separate with deadline and withtimeout

--- adhd/timeout.go
+++ adhd/timeout.go
@@ -10,12 +10,20 @@ type deadlineCtx struct {
 	deadline time.Time
 }
 
-func WithTimeout(parent ADHD, d time.Duration) (ADHD, func()) {
-	deadline := time.Now().Add(d)
+func WithDeadline(parent ADHD, deadline time.Time) (ADHD, func()) {
 	c, cancel := WithCancel(parent)
 	dc := &deadlineCtx{
 		cancelCtx: c.(*cancelCtx),
 		deadline:  deadline,
+	}
+
+	d := time.Until(deadline)
+	if d <= 0 {
+		dc.mu.Lock()
+		dc.err = errors.New("deadline exceeded")
+		close(dc.done)
+		dc.mu.Unlock()
+		return dc, cancel
 	}
 
 	timer := time.AfterFunc(d, func() {
@@ -31,6 +39,10 @@ func WithTimeout(parent ADHD, d time.Duration) (ADHD, func()) {
 		timer.Stop()
 		cancel()
 	}
+}
+
+func WithTimeout(parent ADHD, d time.Duration) (ADHD, func()) {
+	return WithDeadline(parent, time.Now().Add(d))
 }
 
 func (dc *deadlineCtx) Deadline() (deadline time.Time, ok bool) {
