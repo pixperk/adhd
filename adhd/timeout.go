02b210fd66e1588eb0249e07f7e06a5e5cfc59b7
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-09-20 12:58:59 +0530

    timeout

---
+++ adhd/timeout.go
@@ -0,0 +1,34 @@
+package adhd
+
+import (
+	"errors"
+	"time"
+)
+
+type deadlineCtx struct {
+	*cancelCtx
+	deadline time.Time
+}
+
+func WithTimeout(parent ADHD, d time.Duration) (ADHD, func()) {
+	deadline := time.Now().Add(d)
+	c, cancel := WithCancel(parent)
+	dc := &deadlineCtx{
+		cancelCtx: c.(*cancelCtx),
+		deadline:  deadline,
+	}
+
+	timer := time.AfterFunc(d, func() {
+		dc.mu.Lock()
+		defer dc.mu.Unlock()
+		if dc.err == nil {
+			dc.err = errors.New("deadline exceeded")
+			close(dc.done)
+		}
+	})
+
+	return dc, func() {
+		timer.Stop()
+		cancel()
+	}
+}
